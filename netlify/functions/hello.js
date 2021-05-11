be64c1cb12dc8ec422171540ab74a018fafad20b
GtlsGamr <hittarth91@gmail.com>
2021-05-11 19:28:20 +0530

    a

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -11,11 +11,17 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
-  const name = params.comment || "World";
+  //const name = params.comment || "World";
+  
+  
+ var a = {
+"name": params.name,
+"comment": params.comment
+ }
 
   return {
     statusCode: 200,
-    body: `Hello, ${name}`,
+    body: `${a}`,
   };
 };
 
