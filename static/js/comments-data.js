1bdba95dc714d595b0136027d2d8306720c94c64
GtlsGamr <hittarth91@gmail.com>
2021-05-12 22:33:20 +0530

    first comment added

--- static/js/comments-data.js
+++ static/js/comments-data.js
@@ -1,2 +1,2 @@
-var commentsobj = [{},
+var commentsobj = [{"postId":"https://quirky-newton-331f68.netlify.app/post/linear_solution/","time":"Wed May 12 2021 22:32:05 GMT 0530 (India Standard Time)","alias":"GtlsGamr","body":"Test comment for the site. Yes! This was made using my own commenting engine that is aptly named ***[comments.](https://github.com/gtlsgamr/comments)*** "},
 ]
